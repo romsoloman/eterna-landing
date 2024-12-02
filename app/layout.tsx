ea6008cbe4afe1f0304221d88e9ffab4dea461bb
romso <romso@TLV-LT-ROMSO1.local>
2024-12-02 14:01:50 +0200

    feat: Update font in layout to Heebo and add font weight options

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
 import type { ReactNode } from "react"
 import { Metadata } from "next"
-import { Inter } from "next/font/google"
+import { Heebo } from "next/font/google"
 import "../styles/globals.css"
 import Header from "@/components/ui/header"
 import { Container } from "@/components/ui/container"
 
-// If loading a variable font, you don't need to specify the font weight
-const inter = Inter({
-  subsets: ["latin"],
-  display: "optional"
+const heebo = Heebo({
+  subsets: ["hebrew", "latin"],
+  weight: ["100", "300", "400", "500", "700", "900"],
+  display: "swap"
 })
 
 export const metadata: Metadata = {
@@ -18,11 +18,10 @@ export const metadata: Metadata = {
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <html className={inter.className} dir="rtl" lang="he">
+    <html className={heebo.className} dir="rtl" lang="he">
       <head>
         <link rel="icon" href="/favicon.ico" />
       </head>
-
       <body>
         <Header />
         <main>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,21 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
-        foreground: "var(--foreground)",
+        foreground: "var(--foreground)"
       },
-    },
+      fontFamily: {
+        heebo: ["Heebo", "sans-serif"]
+      }
+    }
   },
-  plugins: [],
-} satisfies Config;
+  plugins: []
+} satisfies Config
