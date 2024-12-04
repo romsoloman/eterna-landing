02caba19c13392b7d26be52046684000583fa8e3
romso <romso@ip-10-10-72-86.eu-west-1.compute.internal>
2024-12-04 17:14:58 +0200

    feat: Update AboutUs component with localized texts

--- components/ui/about-us/about-us.tsx
+++ components/ui/about-us/about-us.tsx
@@ -1,18 +1,13 @@
 import React from "react"
-import Divider from "./divider"
+import Divider from "../divider"
+import texts from "./about-us.json"
 
 const AboutUs = () => {
   return (
     <div className="px-8 my-10 flex flex-col gap-8 items-center justify-center">
-      <h1 className="text-3xl">קצת עלינו</h1>
+      <h1 className="text-3xl">{texts.title}</h1>
       <div className="text-center leading-[28px] text-gray-600">
-        ברוכים הבאים ל- Eterna – מסורת, איכות ותשוקה לתכשיטים. אנו דור שלישי
-        בענף היהלומים, וממשיכים את דרכו של סבנו שהיה לוטש יהלומים ואבינו שסוחר
-        ביהלומים. החזון שלנו משלב מורשת משפחתית עשירה עם עיצוב חדשני ודיוק שאין
-        שני לו. במהלך השנים פיתחנו הבנה עמוקה של כל שלבי המסע – מהיהלום הגולמי
-        ועד לתכשיט המושלם, וכל תכשיט שאנחנו יוצרים מספר סיפור של אומנות, איכות
-        ויוקרה. אנחנו כאן כדי ליצור עבורך תכשיטים שמסמלים רגעים בלתי נשכחים
-        ומלווים אותך לאורך החיים, כל זאת עם דגש על שירות אישי ואמין.
+        {texts.description}
       </div>
       <Divider />
     </div>
