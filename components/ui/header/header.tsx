30ff994f31da3f996be22c70f1fcfbaac7cb7394
romsoloman <romsoloman19@gmail.com>
2025-01-13 18:53:31 +0200

    chore: Add accessibility feature to header component

--- components/ui/header/header.tsx
+++ components/ui/header/header.tsx
@@ -1,15 +1,20 @@
 "use client"
 
-import React from "react"
+import React, { useEffect } from "react"
 import Logo from "../../icons/Logo"
 import SocialLinks from "../social-links/social-links"
 import MobileMenu from "./mobile-menu"
 import useHeader from "./header.hooks"
 import HamburgerMenu from "./hamburger-menu"
 import NavigationLinks from "./navigation-links"
+import { Accessibility } from "accessibility"
 
 const Header = () => {
   const { isMenuOpen, toggleMenu } = useHeader()
+
+  useEffect(() => {
+    new Accessibility()
+  }, [])
 
   return (
     <header className="flex flex-row-reverse items-center justify-between px-20  shadow-md h-[90px] max-md:h-[60px] max-md:px-4">
